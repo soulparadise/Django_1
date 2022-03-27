74dba64bbf78a121300db354dc510e63992e3fd3
soulparadise <soulparadiss@gmail.com>
2022-03-27 15:58:28 +0300

    lesson_02 create context for products

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from mainapp.views import index, products
+from mainapp.views import index, products, test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='product')
+    path('products/', products, name='product'),
+    path('test/', test, name='test')
 ]
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -7,5 +7,31 @@ def index(request):
     return render(request, 'mainapp/index.html')
 
 def products(request):
+    context = {'title': "Каталог",
+               'categories': ['Новинки', 'Одежда', 'Обувь', 'Аксессуары', 'Подарки'],
+               'products':[{'name': 'Худи черного цвета с монограммами adidas Originals',
+                            'price': 6090,
+                            'descriprion': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
+                           {'name': 'Синяя куртка The North Face',
+                            'price': 23725,
+                            'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'},
+                           {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
+                            'price': 3390,
+                            'description': 'Материал с плюшевой текстурой. Удобный и мягкий.'},
+                           {'name': 'Черный рюкзак Nike Heritage',
+                            'price': 2340,
+                            'description': 'Плотная ткань. Легкий материал.'},
+                           {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
+                            'price': 13590,
+                            'description': 'Гладкий кожаный верх. Натуральный материал.'},
+                           {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
+                            'price': 2890,
+                            'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.'},
+                           ]}
 
-    return render(request, 'mainapp/products.html')
+    return render(request, 'mainapp/products.html', context)
+
+def test(request):
+    context = {'qqq': '/static/vendor/img/products/Black-Dr-Martens-shoes.png'}
+
+    return render(request, 'mainapp/test.html', context)
