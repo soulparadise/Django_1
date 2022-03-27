63586cdaeaedf5f593672d7896387ca5ae2355c8
soulparadise <soulparadiss@gmail.com>
2022-03-27 16:52:20 +0300

    lesson_02 load static

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from mainapp.views import index, products, test
+from mainapp.views import index, products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', products, name='product'),
-    path('test/', test, name='test')
 ]
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,31 +9,34 @@ def index(request):
     return render(request, 'mainapp/index.html')
 
 def products(request):
-    context = {'title': "Каталог",
-               'categories': ['Новинки', 'Одежда', 'Обувь', 'Аксессуары', 'Подарки'],
+    context = {'date': datetime.datetime.now(),
+               'title': "GeekShop - Каталог",
+               'categories': ['новинки', 'одежда', 'обувь', 'аксессуары', 'подарки'],
                'products':[{'name': 'Худи черного цвета с монограммами adidas Originals',
                             'price': 6090,
-                            'descriprion': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
+                            'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+                            'img': 'vendor/img/products/Adidas-hoodie.png'},
                            {'name': 'Синяя куртка The North Face',
                             'price': 23725,
-                            'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'},
+                            'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
+                            'img': 'vendor/img/products/Blue-jacket-The-North-Face.png'},
                            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
                             'price': 3390,
-                            'description': 'Материал с плюшевой текстурой. Удобный и мягкий.'},
+                            'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
+                            'img': 'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png'},
                            {'name': 'Черный рюкзак Nike Heritage',
                             'price': 2340,
-                            'description': 'Плотная ткань. Легкий материал.'},
+                            'description': 'Плотная ткань. Легкий материал.',
+                            'img': 'vendor/img/products/Black-Nike-Heritage-backpack.png'},
                            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
                             'price': 13590,
-                            'description': 'Гладкий кожаный верх. Натуральный материал.'},
+                            'description': 'Гладкий кожаный верх. Натуральный материал.',
+                            'img': 'vendor/img/products/Black-Dr-Martens-shoes.png'},
                            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
                             'price': 2890,
-                            'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.'},
+                            'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
+                            'img': 'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png'},
                            ]}
 
     return render(request, 'mainapp/products.html', context)
 
-def test(request):
-    context = {'qqq': '/static/vendor/img/products/Black-Dr-Martens-shoes.png'}
-
-    return render(request, 'mainapp/test.html', context)
