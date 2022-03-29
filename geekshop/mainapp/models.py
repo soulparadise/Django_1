beafdbbab4f309e903eee9c73f25457fdeb1f522
soulparadise <soulparadiss@gmail.com>
2022-03-29 23:58:24 +0300

    lesson_03 create models

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ProductCategories(models.Model):
+    """model ProductCategory"""
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """model Product"""
+    name = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='product_images', blank=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(ProductCategories, on_delete=models.CASCADE())
+
+    def __str__(self):
+        return f'{self.name} | {self.category}'
