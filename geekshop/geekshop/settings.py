9fb96085ee6ae9995ffe154a82c1278f0b758d2b
soulparadise <soulparadiss@gmail.com>
2022-04-03 13:31:25 +0300

    lesson_04 authapp add + fill_db

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # my apps
-    'mainapp'
+    'mainapp',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'authapp.User'
--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -24,8 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', include('mainapp.urls', namespace='mainapp')),
+    path('user/', include('authapp.urls', namespace='authapp')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
