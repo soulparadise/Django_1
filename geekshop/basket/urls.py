3bfb165c104d18972d9e2b2bff6c02f19f5b079f
soulparadise <soulparadiss@gmail.com>
2022-04-12 18:29:12 +0300

    lesson_06 complete

--- geekshop/basket/urls.py
+++ geekshop/basket/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from basket.views import basket_add, basket_remove
+from basket.views import basket_add, basket_remove, basket_edit
 
 app_name = 'basket'
 urlpatterns = [
     path('add/<int:id>', basket_add, name='basket_add'),
-    path('remove/<int:basket_id>', basket_remove, name='basket_remove')
+    path('remove/<int:basket_id>', basket_remove, name='basket_remove'),
+    path('edit/<int:id_basket>/<int:quantity>/', basket_edit, name='basket_edit')
 ]
